b7f429ff2f86101a79b7935f29f01903d76ebdd4
Miguel Ángel Urbina <miguel.urbina15.1997@gmail.com>
2021-12-23 12:16:54 -0600

    [TASK] Add test coverage gem (#10)
    * Setup simplecov gem
    
    * Ignore no needed coverage files
    
    * Integrate simplecov report action

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/config/'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
