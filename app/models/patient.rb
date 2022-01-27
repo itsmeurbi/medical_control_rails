5b7cd85ee1a8e1767672c5d22ebeec48a018fe75
Miguel Urbina <itismeurbi@gmail.com>
2022-01-27 13:45:20 -0600

    Add some useless emthods

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,14 @@
 class Patient < ApplicationRecord
+  # TODO: City has to be changed to home_town intead
   validates :name, :birth_date, :city, presence: true
+
+  def example
+    2 + 2
+  end
+
+  def calculate_somthing
+    3.times do
+      Rails.logger.info('Im doing something dude!')
+    end
+  end
 end
